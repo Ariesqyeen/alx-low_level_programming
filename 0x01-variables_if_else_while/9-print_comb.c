7c650ee6cf6be23f3920d3cff33f3f9bd9da37fc
Ariesqyeen <waltersgloria452@gmail.com>
2023-04-23 10:20:26 -0700

    fixed error

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
-{
-	putchar(i + '0');
-}
-	if (i != 9)
-{
-	putchar(',');
-	putchar(' ');
-}
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
