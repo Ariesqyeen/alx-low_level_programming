192274e8a6ecb4889e78a969e39c79ed4392e816
Ariesqyeen <waltersgloria452@gmail.com>
2023-04-24 08:14:27 -0700

    combinations of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: Always 0 if program execution is successful
+ */
+int main(void)
+{
+	int i, x;
+
+	i = 48;
+
+	x = 48;
+
+	while (x < 58)
+	{
+		i = 48;
+
+		while (i < 58)
+		{
+			if (x != i && x < i)
+			{
+				putchar(x);
+				putchar(i);
+				if (i == 57 && x == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
