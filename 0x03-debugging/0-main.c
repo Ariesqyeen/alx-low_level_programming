03694ba8f7ca9e9efc910ce158aec12fc2e89b33
Ariesqyeen <waltersgloria452@gmail.com>
2023-04-27 09:48:05 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void positive_or_negative(int i)
+/**
+ * main - test that the function positive_or_negative
+ * i: print this number
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i)
+
+return (0);
+}
