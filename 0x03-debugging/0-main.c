3d38457d267b751a6799e82d3663fc0dcec37abb
Ariesqyeen <waltersgloria452@gmail.com>
2023-04-27 09:22:25 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * main - test that the function positive_or_negative
+ * i: print this number
  * Return: Always 0
  */
 int main(void)
 {
-	int i;
-	
-	i = 0;
+int i;
 
-	positive_or_negative(i)
-		return (0);
+i = 0;
+
+positive_or_negative(i)
+return (0);
 }
